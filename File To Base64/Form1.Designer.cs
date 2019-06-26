2a0b367b1a043eeae6b26786a54fd42c5af56c3f
Tayeb Madani <38252609+MadLadDZ@users.noreply.github.com>
2019-06-26 17:13:41 +0200

    Add files via upload

---
+++ File To Base64/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace File_To_Base64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(518, 167);
+            this.panel1.TabIndex = 0;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Panel1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Drag And Drop The File To Convert To Base64";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 211);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(517, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Paste Base64 into Clipboard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(370, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save To Text File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.SaveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 271);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Convert File To Base64";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ File To Base64/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_To_Base64
+{
+    //26/06/2019
+    public partial class Form1 : Form
+    {
+        string path { get; set; }     //declares path to use it in other methods
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void Panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);  //gets draganddrop dropped file path in string[]
+            
+            foreach (string file in files)          //just gets path name
+            {
+                path = file;                        //declares path as string
+                textBox1.Clear();                   //clears textbox before pasting it again if dragged&dropped again
+            }
+            textBox1.Paste(path);                   //pastes path in textbox
+
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string newpath = textBox1.Text;  //gets path from text box if changed
+            if (!File.Exists(newpath))
+            {
+                MessageBox.Show("File Not Found\nPlease Check Again");
+            }
+            else
+            {
+                byte[] bytearray = File.ReadAllBytes(newpath);       //reads file 
+                string base64 = Convert.ToBase64String(bytearray);   //converts it to base64 
+                Clipboard.SetText(base64);                           //sets clipboard to base64 of the file
+                MessageBox.Show("Clipboard Set To Base64 of The File");
+            }
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string newpath = textBox1.Text;  //gets path from text box if changed
+            if (!File.Exists(newpath))
+            {
+                MessageBox.Show("File Not Found\nPlease Check Again");
+            }
+            else
+            {
+                byte[] bytearray = File.ReadAllBytes(newpath);      //reads file 
+                string base64 = Convert.ToBase64String(bytearray);  //converts it to base64
+
+                string dir = Path.GetDirectoryName(newpath);     //gets directory of filepath (initial save dir)
+                string filename = Path.GetFileName(newpath);     //gets file name of filepath (initial filename)
+
+                saveFileDialog1.Title = "Save As";                         //dialog title
+                saveFileDialog1.InitialDirectory = dir;                    //where to save file
+                saveFileDialog1.DefaultExt = "txt";                        //default extension
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)       //open savefile dialog
+                {
+                    File.WriteAllText(saveFileDialog1.FileName, base64);   //writes base64 to filename
+                }
+                if (File.Exists(saveFileDialog1.FileName))
+                {
+                    MessageBox.Show("Saved File to Location  " + saveFileDialog1.FileName);
+                }
+                else
+                {
+                    MessageBox.Show("Something Went Wrong\nPlease Try Again");
+                }
+            }
+
+            
+
+        }
+
+        private void SaveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+    }
+}
